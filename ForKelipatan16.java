dbce47ec65894d9462d9e9256f284005d0924c53
LOUISENZR <louisanaza@gmail.com>
2023-10-12 08:03:06 +0700

    first commit

---
+++ ForKelipatan16.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+/**
+ * ForKelipatan16
+ */
+public class ForKelipatan16 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int kelipatan, total, counter ;
+        total = 0;
+        counter = 0;
+
+        System.out.print("Masukan bilangan kelipatan (1-9) : ");
+        kelipatan = scan.nextInt();
+
+        for (int i = 1; i <= 50; i++) {
+            if (i % kelipatan == 0) {
+                total += i;
+                counter++;
+            }
+        }
+        System.out.printf("Banyak bilangan %d dari i sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
+
+        scan.close();
+
+    }
+}
